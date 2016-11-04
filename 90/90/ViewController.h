82fc3dc725fb1fa13e2cc35a098b725a471fa48e
纪高飞 <jigaofei9966@163.com>
2016-11-05 06:17:39 +0800

    90

---
+++ 90/90/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  90
+//
+//  Created by 纪高飞 on 16/11/5.
+//  Copyright © 2016年 纪高飞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 90/90/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  90
+//
+//  Created by 纪高飞 on 16/11/5.
+//  Copyright © 2016年 纪高飞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
